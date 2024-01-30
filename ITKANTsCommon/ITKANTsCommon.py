f149bde8b484fa92ccb49de253bf180f43a782dd
Dženan Zukić <dzenan.zukic@kitware.com>
2024-01-30 14:26:34 -0500

    ENH: Add ITKUltrasoundCommon module from SlicerITKUltrasound
    Combining versions c3779bbea5bc2517b58cf43631e18a86749ec074
    and 525da3869ec78d68a2b052b0e788e53ab4c68d6e, e.g. from branch:
    https://github.com/KitwareMedical/SlicerITKUltrasound/tree/pythonizationWIP2/ITKUltrasoundCommon

---
+++ ITKANTsCommon/ITKANTsCommon.py
@@ -0,0 +1,99 @@
+import logging
+import os
+
+import qt
+import slicer
+from slicer.ScriptedLoadableModule import (
+  ScriptedLoadableModule,
+  ScriptedLoadableModuleLogic,
+)
+
+
+
+class ITKANTsCommon(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "ITK ANTs Common Utilities"
+        self.parent.categories = ["Registration"]
+        self.parent.dependencies = []
+        self.parent.contributors = ["Dženan Zukić (Kitware Inc.)"]
+        self.parent.helpText = "This is a helper module, which contains commonly used ITK functions."
+        self.parent.acknowledgementText = """
+This file was originally developed by Dženan Zukić, Kitware Inc.,
+and was partially funded by NIH grant 5R44CA239830.
+"""
+        # Additional initialization step after application startup is complete
+        # slicer.app.connect("startupCompleted()", preloadITK)
+
+
+
+class ITKANTsCommonLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget.
+    Uses ScriptedLoadableModuleLogic base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self):
+        """
+        Called when the logic class is instantiated. Can be used for initializing member variables.
+        """
+        ScriptedLoadableModuleLogic.__init__(self)
+        self._itk = None
+
+    @property
+    def itk(self):
+        if self._itk is None:
+            logging.info('Importing itk...')
+            self._itk = self.importITK()
+        return self._itk
+
+    def importITK(self, confirmInstallation=True):
+        try:
+            import itk
+        except ModuleNotFoundError:
+            with slicer.util.WaitCursor(), slicer.util.displayPythonShell():
+                itk = self.installITK(confirmInstallation)
+        logging.info(f'ITK {itk.__version__} imported correctly')
+        return itk
+
+
+    @staticmethod
+    def installITK(confirm=True):
+        if confirm and not slicer.app.commandOptions().testingEnabled:
+            install = slicer.util.confirmOkCancelDisplay(
+            'ITK will be downloaded and installed now. The process might take a minute.'
+            )
+            if not install:
+                logging.info('Installation of ITK aborted by the user')
+                return None
+        slicer.util.pip_install('itk-ants>=0.1.0')
+        import itk
+        logging.info(f'ITK {itk.__version__} installed correctly')
+        return itk
+
+
+    def getITKImageFromVolumeNode(self, volumeNode):
+        return slicer.util.itkImageFromVolume(volumeNode)
+
+    def setITKImageToVolumeNode(self, itkImage, outputVolumeNode):
+        slicer.util.updateVolumeFromITKImage(outputVolumeNode, itkImage)
+
+        slicer.util.setSliceViewerLayers(
+            background=outputVolumeNode,
+            fit=True,
+            rotateToVolumePlane=True,
+            )
+
+
+def preloadITK():
+    logic = ITKANTsCommonLogic()
+    logic.importITK(True)
+    logic.itk.ANTSRegistration  # trigger loading of itk-ants DLL
